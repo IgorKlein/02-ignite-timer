0135f3a4c5b43963cd55c18705a5edbd8f1a8105
Igor Klein <igor.klein.sl@gmail.com>
2022-11-22 12:46:45 -0300

    bug fixes on timer reset

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,12 +54,18 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -75,6 +81,7 @@ export function Home() {
 
     setCycles((state) => [...cycles, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -87,6 +94,14 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `Ignite Timer (${minutes}:${seconds})`
+    } else {
+      document.title = `Ignite Timer`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
